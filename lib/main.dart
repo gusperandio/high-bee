ed7b59d4a6f3caf9f1f4d1ebf998b9b737195be2
Gustavo <gustavo.sperandio25@gmail.com>
2025-04-03 09:45:54 -0300

    carousel home started

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:ui';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:high_bee/firebase_options.dart';
+import 'package:infinite_carousel/infinite_carousel.dart';
+import 'package:vertical_card_pager/vertical_card_pager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,27 +17,11 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -44,16 +32,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -76,56 +54,128 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    final List<String> titles = [
+      "RED",
+      "YELLOW",
+      "BLACK",
+      "CYAN",
+      "BLUE",
+      "GREY",
+    ];
+
+    final List<Widget> images = [
+      Container(
+        color: Colors.red,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      Container(
+        color: Colors.yellow,
+      ),
+      Container(
+        color: Colors.black,
+      ),
+      Container(
+        color: Colors.cyan,
+      ),
+      Container(
+        color: Colors.blue,
+      ),
+      Container(
+        color: Colors.grey,
+      ),
+    ];
+    return Scaffold(
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Expanded(
+              child: Container(
+                child: VerticalCardPager(
+                    titles: titles, // required
+                    images: images, // required
+                    textStyle: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold), // optional
+                    onPageChanged: (page) {
+                      // optional
+                    },
+                    onSelectedItem: (index) {
+                      // optional
+                    },
+                    initialPage: 0, // optional
+                    align: ALIGN.RIGHT, // optional
+                    physics: ClampingScrollPhysics() // optional
+                    ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+Widget defaultItemBuilder(BuildContext context, int itemIndex, int realIndex) {
+  final bool isCenter = realIndex == 0;
+  final double zIndex = isCenter ? 2.0 : 1.0; // Item central sempre no topo
+
+  return Transform.translate(
+    offset: Offset(0, isCenter ? 0 : (realIndex > 0 ? -120 : 120)),
+    child: Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            spreadRadius: 1,
+            offset: const Offset(0, 5),
+          ),
+        ],
+        border: isCenter
+            ? Border.all(
+                color: Colors.blueAccent,
+                width: 2,
+              )
+            : null,
+      ),
+      width: 250,
+      height: 400,
+      child: Center(
+        child: Image.asset('assets/images/Screenshot_$itemIndex.png',
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.fitHeight),
+      ),
+    ),
+  );
+}
+
+class CarouselDemo extends StatelessWidget {
+  final InfiniteScrollController _controller = InfiniteScrollController();
+
+  CarouselDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Carrossel Vertical')),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 80),
+          child: SizedBox(
+            height: double.infinity,
+            child: InfiniteCarousel.builder(
+                itemCount: 5,
+                itemExtent: 400,
+                controller: _controller,
+                axisDirection: Axis.vertical,
+                loop: true,
+                onIndexChanged: (index) => print('Índice atual: $index'),
+                itemBuilder: defaultItemBuilder),
+          ),
+        ),
+      ),
     );
   }
 }
